62c81f2e3b0996adb5319d15a10b6cee79183901
Sarvesh Sawant <name.sarveshsawant@gmail.com>
2024-01-05 13:28:11 +0530

    default spring security

---
+++ basic-security/src/main/java/com/sample/basicsecurity/BasicSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sample.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSecurityApplication.class, args);
+	}
+
+}
