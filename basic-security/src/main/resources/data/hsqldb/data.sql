7d4bfdd3cfa76a3be5bbb88272d671aed9bfa1a7
Sarvesh Sawant <name.sarveshsawant@gmail.com>
2024-01-10 11:01:48 +0530

    jdbc-auth bcrypt password in db

--- basic-security/src/main/resources/data/hsqldb/data.sql
+++ basic-security/src/main/resources/data/hsqldb/data.sql
@@ -1,8 +1,16 @@
+-- INSERT INTO users
+-- VALUES
+-- ('spring','{noop}hello-spring',1),
+-- ('john','{noop}hello-john',1),
+-- ('tom','{noop}hello-tom',1);
+
+-- Bcrpt Password
 INSERT INTO users
 VALUES
-('spring','{noop}hello-spring',1),
-('john','{noop}hello-spring',1),
-('tom','{noop}hello-tom',1);
+('spring','{bcrypt}$2a$10$gMALp3vlGqUNnyfuGlU45e2AzIhWZFUzaGhRptT2wTHMZ1VEoI5R6',1),
+('john','{bcrypt}$2a$10$nwNgLb0OBpFNvLu6bm4x2ejXHkYN2anXTtHQZhXYs3hw0uzTCmGCi',1),
+('tom','{bcrypt}$2a$10$PPVc9I1HECLh/3bfRjdgguUTcoJU9uUTQF09GsnLtm5pMW2LLfnGu',1);
+
 
 INSERT INTO authorities
 VALUES
--- basic-security/src/main/resources/data/hsqldb/schema.sql
+++ basic-security/src/main/resources/data/hsqldb/schema.sql
@@ -2,16 +2,16 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     username varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
+    password varchar(68) NOT NULL,
     enabled tinyint NOT NULL,
     PRIMARY KEY (username)
-)
+);
 
-DROP TABLE IF EXISTS authoriries
+DROP TABLE IF EXISTS authoriries;
 
 CREATE TABLE authorities (
     username varchar(50) NOT NULL,
     authority varchar(50) NOT NULL
     -- UNIQUE KEY authorities_idx_1 (username,authority),
     -- CONSTRAINT authorities_idfk_1 FOREIGN KEY (username) REFERENCES users(username)
-)
+);
